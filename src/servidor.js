7a101a41dba93a9e030d22b55c2e4d784d522c44
Renan <renancorreia730@gmail.com>
2026-09-02 16:15:02 -0300

    Faltou adicionar no servidor( Adicionei as rotas no servidor)

--- src/servidor.js
+++ src/servidor.js
@@ -1,18 +1,23 @@
-import express, { json } from "express";
+import {express, json } from "express";
 
-import usuario from "./routes/rotaUsuario.js";
+import rotaUsuario from "./routes/rotaUsuario.js";
 import rotaEvento from "./routes/rotaEvento.js";
 import rotaInscricao from "./routes/rotaInscricao.js";
+import rotaCertificado from "./routes/rotaCertificado.js";
+import rotaRelatorio from "./routes/rotaRelatorio.js";
 import rotaCertificado from "./routes/rotaCertificado.js";
 
 const app = express();
 
 app.use(json());
 
-app.use("/api/usuarios", usuario);
+app.use("/api/usuarios", rotaUsuario);
 app.use("/api/eventos", rotaEvento);
 app.use("/api/participacao", rotaInscricao);
 app.use("/api/certificados", rotaCertificado);
+app.use("/api/relatorios", rotaRelatorio);
+app.use("/api/certificados", rotaCertificado);
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
