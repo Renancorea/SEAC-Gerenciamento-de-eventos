6ffa897165956d45b606df12a4d001468c36955d
Renan <renancorreia730@gmail.com>
2026-09-09 19:37:19 -0300

    Rota iscrição

--- src/routes/rotaInscricao.js
+++ src/routes/rotaInscricao.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { realizarInscricao, registrarPresenca, vizualizarInscritos } from "../controllers/controladorInscricao.js";
+import { realizarInscricao, registrarPresenca, vizualizarInscritos,verificarInscricaoUsuario, vizualizarPresentes} from "../controllers/controladorInscricao.js";
 
 const router = Router();
 
 router.post("/inscricao", realizarInscricao);
 router.put("/presenca", registrarPresenca);
 router.get("/eventos/:eventoId/inscritos", vizualizarInscritos);
+router.get("/eventos/:eventoId/presentes", vizualizarPresentes);
+router.get("/usuario/:userId/inscricao", verificarInscricaoUsuario);
 
 export default router;
